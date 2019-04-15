29daebea2eb054ae2bfd1595b159d0fef1009761
恒锋 姚 <TINGFENG20@outlook.com>
2019-04-16 00:57:05 +0800

    添加项目文件。

---
+++ ASP.NET_API/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP.NET_API.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpPost]
+        public string getss()
+        {
+            Msg m = new Msg();
+            m.ID = 1;
+            m.Name = "姚恒锋";
+            return JsonConvert.SerializeObject(m);
+        }
+        [HttpGet]
+        public string getsss()
+        {
+            Msg m = new Msg();
+            m.ID = 1;
+            m.Name = "姚恒锋111111111get";
+            return JsonConvert.SerializeObject(m);
+        }
+
+
+        [HttpPost]
+        public string Pp()
+        {
+            Msg m = new Msg();
+            m.ID = 1;
+            m.Name = "dfgdfgd";
+            return JsonConvert.SerializeObject(m);
+        }
+
+        class Msg
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
